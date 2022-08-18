4f8679161ed78951bd11d6709bfa0898ce9acb52
Dmitry <51984125+dmitrykorochkin@users.noreply.github.com>
2022-08-18 22:08:06 +0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
 			<div className="footer-logo"></div>
 			<div className="footer-company"><span>© 2020 XXXcompany. Все права защищены.</span></div>
 			<a href="tel:888" className="footer-phone"></a>
-			<div className="footer-websurfer"><span class="footer-websurfer_build">Сделано</span> <a href="#">Ваше имя</a></div>
+			<div className="footer-websurfer"><span className="footer-websurfer_build">Сделано</span> <a href="#">Ваше имя</a></div>
 			<div className="footer-phonelink"><a href="tel:888">+7(962)556-1234</a></div>
 		</div>
 	</footer>
